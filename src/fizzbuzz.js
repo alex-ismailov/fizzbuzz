0b7efe1a55a0faa0b1a09a7e12727eeb56c397be
alex-ismailov <alexander.is.dev@gmail.com>
2019-11-26 20:45:05 +0300

    Short fizzBuzz solution added.

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -11,3 +11,11 @@
     }
   }
 })();
+
+console.log('*******************************');
+
+(function fizzBuzz() {
+  for (let i = 1; i <= 100;) {
+    console.log((++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || i);
+  }
+})();
